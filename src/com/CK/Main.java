8cefc3ea720a4431fcf367a6a54b877da366a39f
CK <kainingc@usc.edu>
2019-06-23 10:59:24 -0700

    2019年6月23日10:59:21

--- src/com/CK/Main.java
+++ src/com/CK/Main.java
@@ -1,18 +1,34 @@
 package com.CK;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        int[] myIntArray = new int[10];
-        for (int i = 0; i < myIntArray.length; i++){
-            myIntArray[i] = i * 10;
+        int[] myIntegers = getIntegers(5);
+        for (int i=0; i<myIntegers.length; i++){
+            System.out.println("Element " + i + ", typed values was "+ myIntegers[i]);
         }
-        printArray(myIntArray);
+        System.out.println("The average is "+ getAverage(myIntegers));
     }
 
-    public static void  printArray(int[] Array){
-        for (int i = 0; i < Array.length; i++){
-            System.out.println(Array[i]);;
+    public static int[] getIntegers(int number){
+        System.out.println("Enter" + number + "integer values.\r");
+        int[] values = new int[number];
+
+        for (int i = 0; i < values.length; i++){
+            values[i] = scanner.nextInt();
         }
+        return values;
+    }
+
+    public static double getAverage(int[] array){
+        int sum = 0;
+        for (int i=0; i < array.length; i++){
+            sum+=array[i];
+        }
+        return (double)sum/(double)array.length;
     }
 }
